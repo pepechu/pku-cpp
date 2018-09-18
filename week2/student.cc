9efea016cf8ed55c52451282223a6a59f93d7209
pepechu <qingyuy@andrew.cmu.edu>
2018-09-17 23:17:11 -0400

    week2 hw fixed

--- week2/student.cc
+++ week2/student.cc
@@ -1,36 +1,37 @@
 /* student */
-#include <string>
 #include <math.h>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 class student {
 public:
-	student(string name,
+	student(char* name,
 		int age,
-		string number,
+		char* number,
 		int ave_year_1,
 		int ave_year_2,
 		int ave_year_3,
 		int ave_year_4) :
-		name_(name),
 		age_(age),
-		number_(number),
 		ave_year_1_(ave_year_1),
 		ave_year_2_(ave_year_2),
 		ave_year_3_(ave_year_3),
-		ave_year_4_(ave_year_4) {}
+		ave_year_4_(ave_year_4) {
+			strcpy(name_, name);
+			strcpy(number_, number);
+		}
 	int CalculateAve() {
 		return (int) ((ave_year_1_ * 1.0 + ave_year_2_ + ave_year_3_ + ave_year_4_) / 4);
 	}
-	string GetName() { return name_; }
+	char* GetName() { return name_; }
 	int GetAge() { return age_; }
-	string GetNumber() { return number_; }
+	char* GetNumber() { return number_; }
 private:
-	string name_;
+	char name_[30];
 	int age_;
-	string number_;
+	char number_[10];
 	int ave_year_1_;
 	int ave_year_2_;
 	int ave_year_3_;
@@ -38,14 +39,18 @@ private:
 };
 
 int main() {
-	string name;
+	char name[30];
 	int age;
-	string number;
+	char number[10];
 	int ave_year_1;
 	int ave_year_2;
 	int ave_year_3;
 	int ave_year_4;
-	cin >> name >> age >> number >> ave_year_1 >> ave_year_2 >> ave_year_3 >> ave_year_4;
+	char c;
+	cin.getline(name, 30, ',');
+	cin >> age >> c;
+	cin.getline(number, 10, ',');
+	cin >> ave_year_1 >> c >>  ave_year_2 >> c >> ave_year_3 >> c >> ave_year_4;
 	student s(name, age, number, ave_year_1, ave_year_2, ave_year_3, ave_year_4);
 	cout << s.GetName() << "," << s.GetAge() << "," 
 		<< s.GetNumber() << "," << s.CalculateAve() << endl;
